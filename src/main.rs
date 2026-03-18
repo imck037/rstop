5fb04938ebd6ec420f0f5ce081c26b7a2635a44b
imck037 <chandradhar.2003@gmail.com>
2026-03-18 18:58:41 +0530

    per cpu core usage are added...

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::{io, time::Duration};
+use std::{
+    io,
+    time::{Duration, Instant},
+};
 
 use crossterm::{
     self,
@@ -11,7 +14,7 @@ use ratatui::{
     backend::CrosstermBackend,
     layout::{Constraint, Direction, Layout},
     style::{Color, Modifier, Style},
-    widgets::{Block, Borders, Paragraph, Row, Table, TableState},
+    widgets::{Block, Borders, Gauge, Paragraph, Row, Table, TableState},
 };
 use sysinfo::System;
 
@@ -33,6 +36,9 @@ fn main() -> Result<(), io::Error> {
 
     let mut sorting_mode = SortingMode::Memory;
     let mut selected = 0usize;
+    let mb = 1024 * 1024;
+    let refresh_rate = Duration::from_millis(200);
+    let last_refresh = Instant::now();
     loop {
         sys.refresh_all();
         let mut processes: Vec<_> = sys.processes().iter().collect();
@@ -40,24 +46,44 @@ fn main() -> Result<(), io::Error> {
         terminal.draw(|f| {
             let area = f.area();
 
+            let cpus = sys.cpus();
             let chunk = Layout::default()
                 .direction(Direction::Vertical)
-                .constraints([Constraint::Length(5), Constraint::Min(10)])
+                .constraints([
+                    Constraint::Length(3),
+                    Constraint::Length((cpus.len()) as u16),
+                    Constraint::Min(10),
+                ])
                 .split(area);
 
-            let cpu = sys.global_cpu_usage();
-            let used_memory = sys.used_memory() / 1024 / 1024;
-            let total_memory = sys.total_memory() / 1024 / 1024;
+            let used_memory = sys.used_memory() / mb;
+            let total_memory = sys.total_memory() / mb;
 
-            let stat = Paragraph::new(format!(
-                "CPU: {:.2}%\nMemory: {} MB/ {} MB",
-                cpu, used_memory, total_memory
-            ))
-            .block(
-                Block::default()
-                    .title("System status")
-                    .borders(Borders::all()),
-            );
+            let cpu_chunk = Layout::default()
+                .direction(Direction::Vertical)
+                .constraints(cpus.iter().map(|_| Constraint::Length(1)))
+                .split(chunk[1]);
+
+            for (i, cpu) in cpus.iter().enumerate() {
+                let usage = cpu.cpu_usage();
+                let name = cpu.name();
+                let bar_layout = Layout::horizontal([Constraint::Length(10), Constraint::Min(20)])
+                    .split(cpu_chunk[i]);
+                let gauge = Gauge::default()
+                    .style(Style::new().bg(Color::Reset).fg(Color::DarkGray))
+                    .block(Block::default().borders(Borders::empty()))
+                    .percent(usage.round() as u16)
+                    .label(format!("{:.1}%", usage));
+                f.render_widget(name, bar_layout[0]);
+                f.render_widget(gauge, bar_layout[1]);
+            }
+
+            let stat = Paragraph::new(format!("Memory: {} MB/ {} MB", used_memory, total_memory))
+                .block(
+                    Block::default()
+                        .title("System status")
+                        .borders(Borders::all()),
+                );
 
             f.render_widget(stat, chunk[0]);
 
@@ -71,7 +97,7 @@ fn main() -> Result<(), io::Error> {
                 }
             }
 
-            let visible_height = chunk[1].height as usize - 3;
+            let visible_height = chunk[2].height as usize - 3;
 
             let start = if selected >= visible_height {
                 selected - visible_height + 1
@@ -83,11 +109,28 @@ fn main() -> Result<(), io::Error> {
 
             let rows: Vec<Row> = visible
                 .map(|(pid, process)| {
+                    let display_name = if !process.cmd().is_empty() {
+                        let name: Vec<&str> = process
+                            .cmd()
+                            .iter()
+                            .filter_map(|part| part.to_str())
+                            .collect();
+                        format!("{}", name.join(" "))
+                    } else if process.exe().unwrap() == "" {
+                        format!("{:?}", process.name())
+                    } else {
+                        format!("{:?}", process.exe())
+                    };
                     Row::new(vec![
                         pid.to_string(),
-                        format!("{:?}", process.name()),
+                        format!("{}", display_name),
                         format!("{:.2}", process.cpu_usage()),
-                        format!("{}", process.memory() / 1024 / 1024),
+                        format!("{:.2}", process.virtual_memory() / mb),
+                        format!("{}", process.memory() / mb),
+                        format!("{}", process.status()),
+                        format!("{}", process.disk_usage().total_read_bytes / mb),
+                        format!("{}", process.disk_usage().total_written_bytes / mb),
+                        format!("{:?}", process.start_time()),
                     ])
                 })
                 .collect();
@@ -98,14 +141,30 @@ fn main() -> Result<(), io::Error> {
                 rows,
                 [
                     Constraint::Length(7),
-                    Constraint::Length(20),
+                    Constraint::Length(60),
+                    Constraint::Length(10),
+                    Constraint::Length(10),
+                    Constraint::Length(10),
+                    Constraint::Length(10),
+                    Constraint::Length(10),
                     Constraint::Length(10),
                     Constraint::Length(10),
                 ],
             )
             .row_highlight_style(Style::default().bg(Color::DarkGray))
             .header(
-                Row::new(vec!["PID", "Process", "CPU %", "Memory"]).style(
+                Row::new(vec![
+                    "PID",
+                    "Process",
+                    "CPU %",
+                    "Virt Mem",
+                    "Memory",
+                    "Status",
+                    "Disk Read",
+                    "Disk Write",
+                    "Start Time",
+                ])
+                .style(
                     Style::default()
                         .black()
                         .add_modifier(Modifier::BOLD)
@@ -114,10 +173,10 @@ fn main() -> Result<(), io::Error> {
             )
             .block(Block::default().title("Process").borders(Borders::all()));
 
-            f.render_stateful_widget(table, chunk[1], &mut state);
+            f.render_stateful_widget(table, chunk[2], &mut state);
         })?;
 
-        if event::poll(Duration::from_millis(200))? {
+        if event::poll(Duration::from_millis(0))? {
             if let Event::Key(key) = event::read()? {
                 match key.code {
                     KeyCode::Down | KeyCode::Char('j') => {
@@ -142,6 +201,10 @@ fn main() -> Result<(), io::Error> {
                     _ => {}
                 }
             }
+        }
+
+        if last_refresh.elapsed() >= refresh_rate {
+            sys.refresh_all();
         }
     }
     disable_raw_mode()?;
