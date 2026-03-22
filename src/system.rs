30cdf2254497b00d6bff6ebe044633db1fa7fa36
imck037 <chandradhar.2003@gmail.com>
2026-03-22 21:13:56 +0530

    added: cpu usage per core

--- src/system.rs
+++ src/system.rs
@@ -1,5 +1,53 @@
 use std::fs;
 
+pub struct CpuState {
+    id: String,
+    idle: usize,
+    total: usize,
+}
+
+pub struct CpuUsage {
+    pub id: String,
+    pub usage: f64,
+}
+
+pub fn get_cpu_stat() -> Vec<CpuState> {
+    let cpu_details = fs::read_to_string("/proc/stat").unwrap();
+    let mut cpus: Vec<CpuState> = Vec::new();
+    for line in cpu_details.lines() {
+        if line.starts_with("cpu") {
+            let parts: Vec<&str> = line.split_whitespace().collect();
+            let id = parts[0].to_string();
+            let values: Vec<usize> = parts[1..]
+                .iter()
+                .filter_map(|value| value.parse::<usize>().ok())
+                .collect();
+            let idle = values[3] + values[4];
+            let total: usize = values.iter().sum();
+            cpus.push(CpuState { id, idle, total });
+        }
+    }
+    cpus
+}
+
+pub fn get_cpu_usage(previous: &Vec<CpuState>, current: &Vec<CpuState>) -> Vec<CpuUsage> {
+    let mut cpus: Vec<CpuUsage> = Vec::new();
+
+    for (prev, curr) in previous.iter().zip(current.iter()) {
+        let idle_delta = curr.idle - prev.idle;
+        let total_delta = curr.total - prev.total;
+        let id = curr.id.to_string();
+
+        let usage = if total_delta == 0 {
+            0.0
+        } else {
+            (1.0 - idle_delta as f64 / total_delta as f64) * 100.0
+        };
+        cpus.push(CpuUsage { id, usage });
+    }
+    cpus
+}
+
 pub fn get_uptime() -> String {
     let read_uptime = fs::read_to_string("/proc/uptime").unwrap();
     let uptime_stat: Vec<&str> = read_uptime.split(".").collect();
