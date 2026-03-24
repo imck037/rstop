e727795446425d44ca351bbbf5ccebce0effb2b2
imck037 <chandradhar.2003@gmail.com>
2026-03-24 21:12:40 +0530

    fetures: kill functionality is added

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ use crossterm::{
     execute,
     terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
 };
+use ratatui::layout::Rect;
+use ratatui::style::Stylize;
+use ratatui::widgets::Clear;
 use ratatui::{
     Terminal,
     backend::CrosstermBackend,
@@ -25,9 +28,25 @@ enum SortingMode {
     Memory,
 }
 
+enum UiMode {
+    Normal,
+    SignalMenu,
+}
+
 struct ProcessCache {
     prev_proc: HashMap<usize, usize>,
     prev_total: usize,
+}
+
+struct SignalOption {
+    name: &'static str,
+    value: i32,
+}
+
+fn send_signal(pid: i32, signal: i32) {
+    unsafe {
+        libc::kill(pid, signal);
+    }
 }
 
 fn main() -> Result<(), io::Error> {
@@ -42,6 +61,7 @@ fn main() -> Result<(), io::Error> {
 
     let mut sorting_mode = SortingMode::Memory;
     let mut selected = 0usize;
+    let mut signal_selected = 0usize;
 
     let mut proc_cache = ProcessCache {
         prev_proc: HashMap::new(),
@@ -49,6 +69,27 @@ fn main() -> Result<(), io::Error> {
     };
 
     let core_count = prev_cpus.len() - 1;
+
+    let mut ui_mode = UiMode::Normal;
+
+    const SIGNALS: &[SignalOption] = &[
+        SignalOption {
+            name: "SIGTERM",
+            value: libc::SIGTERM,
+        },
+        SignalOption {
+            name: "SIGKILL",
+            value: libc::SIGKILL,
+        },
+        SignalOption {
+            name: "SIGSTOP",
+            value: libc::SIGSTOP,
+        },
+        SignalOption {
+            name: "SIGCONT",
+            value: libc::SIGCONT,
+        },
+    ];
 
     loop {
         let mut processes = proc::get_process();
@@ -180,31 +221,86 @@ fn main() -> Result<(), io::Error> {
             .block(Block::default().title("Process").borders(Borders::all()));
 
             frame.render_stateful_widget(table, layout[1], &mut state);
+
+            if let UiMode::SignalMenu = ui_mode {
+                let signal_area = centered_layout(20, 20, area);
+
+                let rows: Vec<Row> = SIGNALS
+                    .iter()
+                    .enumerate()
+                    .map(|(i, s)| {
+                        let style = if i == signal_selected {
+                            Style::default().bg(Color::Gray)
+                        } else {
+                            Style::default()
+                        };
+
+                        Row::new(vec![s.name]).style(style)
+                    })
+                    .collect();
+
+                let signal_table = Table::new(rows, [Constraint::Percentage(100)]).block(
+                    Block::default()
+                        .title("Kill Signal")
+                        .borders(Borders::all()),
+                );
+                frame.render_widget(Clear, area);
+                frame.render_widget(signal_table, signal_area);
+            }
         })?;
 
         if event::poll(Duration::from_millis(1000))? {
             if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Down | KeyCode::Char('j') => {
-                        if selected < processes.len().saturating_sub(1) {
-                            selected += 1;
+                match ui_mode {
+                    UiMode::Normal => match key.code {
+                        KeyCode::Down | KeyCode::Char('j') => {
+                            if selected < processes.len().saturating_sub(1) {
+                                selected += 1;
+                            }
                         }
-                    }
-                    KeyCode::Up | KeyCode::Char('k') => {
-                        if selected > 0 {
-                            selected -= 1;
+                        KeyCode::Up | KeyCode::Char('k') => {
+                            if selected > 0 {
+                                selected -= 1;
+                            }
                         }
-                    }
-                    KeyCode::Char('c') => {
-                        sorting_mode = SortingMode::Cpu;
-                    }
-                    KeyCode::Char('m') => {
-                        sorting_mode = SortingMode::Memory;
-                    }
-                    KeyCode::Char('q') => {
-                        break;
-                    }
-                    _ => {}
+                        KeyCode::Char('c') => {
+                            sorting_mode = SortingMode::Cpu;
+                        }
+                        KeyCode::Char('m') => {
+                            sorting_mode = SortingMode::Memory;
+                        }
+                        KeyCode::Enter => {
+                            ui_mode = UiMode::SignalMenu;
+                            signal_selected = 0;
+                        }
+                        KeyCode::Char('q') => {
+                            break;
+                        }
+                        _ => {}
+                    },
+                    UiMode::SignalMenu => match key.code {
+                        KeyCode::Down | KeyCode::Char('j') => {
+                            if signal_selected < processes.len().saturating_sub(1) {
+                                signal_selected += 1;
+                            }
+                        }
+                        KeyCode::Up | KeyCode::Char('k') => {
+                            if signal_selected > 0 {
+                                signal_selected -= 1;
+                            }
+                        }
+                        KeyCode::Enter => {
+                            if let Some(p) = processes.get(selected) {
+                                let sig = SIGNALS[signal_selected].value;
+                                send_signal(p.pid.try_into().unwrap(), sig);
+                            }
+                            ui_mode = UiMode::Normal;
+                        }
+                        KeyCode::Esc | KeyCode::Char('q') => {
+                            ui_mode = UiMode::Normal;
+                        }
+                        _ => {}
+                    },
                 }
             }
         }
@@ -213,4 +309,23 @@ fn main() -> Result<(), io::Error> {
     execute!(terminal.backend_mut(), LeaveAlternateScreen)?;
     terminal.show_cursor()?;
     Ok(())
+}
+
+fn centered_layout(x: u16, y: u16, r: Rect) -> Rect {
+    let popup_layout = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Percentage((100 - y) / 2),
+            Constraint::Percentage(y),
+            Constraint::Percentage((100 - y) / 2),
+        ])
+        .split(r);
+    Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage((100 - x) / 2),
+            Constraint::Percentage(x),
+            Constraint::Percentage((100 - x) / 2),
+        ])
+        .split(popup_layout[1])[1]
 }
